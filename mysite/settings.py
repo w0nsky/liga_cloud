b7ba4381462dd0db64ae1220d232f49b8f8a846f
Kamil Kaźmierczak <kamilkazmierczak10@gmail.com>
2025-06-02 22:25:11 +0200

    Fixed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-!@#%$^&*()_+')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [ config('ALLOWED_HOSTS', default='localhost').split(',') ]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 
 # Application definition
