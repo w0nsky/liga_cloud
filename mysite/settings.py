c9f3bcc2072e669272995ee7d241f85a9a6a2a38
Kamil Kaźmierczak <kamilkazmierczak10@gmail.com>
2025-06-02 22:34:19 +0200

    fix: hardcoded ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-!@#%$^&*()_+')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+ALLOWED_HOSTS = ['liga-app-g3ekbec6c6htgpf5.polandcentral-01.azurewebsites.net','localhost','127.0.0.1',]
 
 
 # Application definition
